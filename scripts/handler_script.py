0f083555d05bb062f7d2f9e73d8bf0219693c4b7
Arkhymadhe <emoekabuoshomah@gmail.com>
2025-02-13 12:22:46 +0100

    Update handler_script.py

--- scripts/handler_script.py
+++ scripts/handler_script.py
@@ -2,11 +2,13 @@
 
 """Handler script for inference."""
 
+import numpy as np
+import cv2
+
 import torch
 from ts.torch_handler.image_classifier import ImageClassifier
 
 from torchvision import transforms as T
-from imageio.v3 import imread
 
 __all__ = [
     "CassavaClassifier"
@@ -33,18 +35,46 @@ class CassavaClassifier(ImageClassifier):
     def __init__(self, ):
         super().__init__()
 
+
+    def initialize(self, context):
+        self._context = context
+        self.manifest = context.manifest
+
+        model_file = self.manifest['model']['serializedFile']
+        model_dir = context.system_properties.get("model_dir")
+
+        import os
+        model_path = os.path.join(model_dir, model_file)
+
+
+        self.model = torch.load(model_path)
+        self.model.eval()
+        self.initialized = True
+
     @torch.no_grad()
     def preprocess(self, data):
-        data = data[0]
-        data = bytes(data["body"])
 
-        data = imread(data)
+        print(data)
+        data = data[0]
+
+        data = np.frombuffer(data["data"], dtype=np.uint8)
+        data = cv2.imdecode(data, cv2.IMREAD_COLOR)
+        data = np.asarray(data, dtype="uint8")
+
         data = torch.tensor(data, dtype = torch.float32)
 
         data = data.permute(2, 0, 1)
         data = self.transform(data).unsqueeze(0)
 
         return data.cuda()
+
+    @torch.no_grad()
+    def inference(self, data):
+        if len(data.shape) < 4:
+            data = data.unsqueeze(0)
+
+        prediction = self.model(data)
+        return prediction
 
     @torch.no_grad()
     def postprocess(self, data):
